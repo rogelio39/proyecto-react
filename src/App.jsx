0dc1814a60e45e27b04e621a03b9c04b90b62b1b
Rogelio Suleta <andresrogesu@gmail.com>
2023-05-09 16:02:02 -0300

    se dejaron solos los componentes que iran para el tp final

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 
 import './App.css';
-import ProductCard from './components/ProductCard/ProductCard';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
-import ItemCount from './components/ItemCount/ItemCount';
-import Vista from './components/Vista/Vista';
-import Mensaje from './components/Mensaje/Mensaje';
-import Categorias from './components/Categorias/Categorias';
+import JsonPlaceHolder from './components/JasonPlaceHolder/JsonPlaceHolder';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ProductCard />
       <ItemListContainer greeting='Paradisi'/>
-      <Categorias/>
-      <ItemCount inicial={1} stock={10}/>
-      <Vista/>
-      <Mensaje/>
+      <JsonPlaceHolder/>
 
     </div >
   );
